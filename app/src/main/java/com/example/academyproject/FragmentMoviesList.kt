c1217735ad9b09a2b34bfe4eb7da0a15bc972119
Nikolai Tsygankov <star-lord3000@mail.ru>
2020-12-03 23:31:06 +0300

    Update Project

---
+++ app/src/main/java/com/example/academyproject/FragmentMoviesList.kt
@@ -0,0 +1,25 @@
+package com.example.academyproject
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import androidx.navigation.fragment.findNavController
+
+class FragmentMoviesList : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View = inflater.inflate(R.layout.fragment_movies_list, container, false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        view.findViewById<ImageView>(R.id.mask_avengers).setOnClickListener {
+            findNavController().navigate(R.id.action_FragmentMoviesList_to_FragmentMoviesDetails)
+        }
+    }
+}
